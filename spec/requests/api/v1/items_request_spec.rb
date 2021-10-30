d593b9e7f73af459d7d1c19f98c52dc15f6c5b36
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-10-29 22:03:43 -0700

    Add spec for item patch

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+describe 'Items API' do
+  it 'can update an existing book' do
+      id = create(:item).id
+      previous_name = Item.last.name
+      item_params = { name: "Item Name"}
+      headers = {"CONTENT_TYPE" => "application/json"}
+
+      patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({item: item_params})
+      item = Item.find_by(id: id)
+
+      expect(response).to be_successful
+      expect(item.name).to_not eq(previous_name)
+      expect(item.name).to eq("Item Name")
+    end
+end
